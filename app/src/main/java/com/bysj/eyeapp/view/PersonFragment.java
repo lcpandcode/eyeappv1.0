04d4d5e5fca5e44fb3c373645a90a5f297495135
lcp <1140823948@qq.com>
2018-02-02 18:39:51 +0800

    bug修复

--- app/src/main/java/com/bysj/eyeapp/view/PersonFragment.java
+++ app/src/main/java/com/bysj/eyeapp/view/PersonFragment.java
@@ -1,12 +1,15 @@
 package com.bysj.eyeapp.view;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class PersonFragment extends Fragment {
+	private View thisView;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -16,6 +19,13 @@ public class PersonFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view= inflater.inflate(R.layout.fragment_person, null);
+		thisView = view;
+		init();
 		return view;
+	}
+
+	private void init(){
+		Intent intent = new Intent(getActivity(),PersonInfo.class);
+	   startActivity(intent);
 	}
 }
